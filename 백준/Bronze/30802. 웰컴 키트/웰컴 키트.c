95b191629c657542be4661250824353843d504c5
jeong <jty7231@naver.com>
2025-04-20 21:32:28 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int* pn;
+
+	pn = &n;
+
+	scanf("%d", pn);
+
+	int size[6];
+	int* ps;
+	ps = size;
+
+	for (int i = 0; i < 6; i++)
+	{
+		scanf("%d", ps++);
+	}
+
+	int t, p;
+	
+	int* pt, * pp;
+
+	pt = &t;
+	pp = &p;
+
+	scanf("%d %d", pt, pp);
+
+
+	int t_count = 0;
+	int* pt_count = &t_count;
+
+	for (int i = 0; i < 6; i++)
+	{
+		*pt_count += size[i] / *pt;
+
+		if (size[i] % t != 0)
+		{
+			t_count += 1;
+		}
+	}
+
+	int p_count;
+	p_count = *pn / *pp;
+	int p_count_each;
+	p_count_each = *pn % *pp;
+
+
+	printf("%d\n", t_count);
+	printf("%d %d\n", p_count, p_count_each);
+
+	return 0;
+}
