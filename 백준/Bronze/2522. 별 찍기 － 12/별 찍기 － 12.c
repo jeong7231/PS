3a3654e43ae88b7df97c12617e5c098c97bb92a4
jeong <jty7231@naver.com>
2025-05-23 23:44:29 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    int line = 2 * n - 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > 0; j--)
+        {
+            if (j > i + 1)
+                printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n; i < line; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            if (j <= i - n)
+                printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
