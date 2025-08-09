2b4d6a4dab95cad487f9b40416ca1d3ff3185c73
Taeyun Jeong <jty7231@naver.com>
2025-08-09 14:46:07 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <deque>
+#include <utility>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    deque<pair<int, int>> dq;
+    for (int i = 1; i <= N; i++)
+    {
+        int x;
+        cin >> x;
+        dq.push_back({i, x});
+    }
+
+    while (!dq.empty())
+    {
+        int k = dq.front().second;
+        cout << dq.front().first << " ";
+        dq.pop_front();
+        if (dq.empty())
+            break;
+
+        if (k < 0)
+        {
+            int temp = (-k) % (int)dq.size();
+            for (int i = 0; i < temp; i++)
+            {
+                dq.push_front(dq.back());
+                dq.pop_back();
+            }
+        }
+        else if (k > 0)
+        {
+            int temp = (k - 1) % (int)dq.size();
+            for (int i = 0; i < temp; i++)
+            {
+                dq.push_back(dq.front());
+                dq.pop_front();
+            }
+        }
+    }
+    return 0;
+}
