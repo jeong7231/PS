47eab762fa4caed072ce32e495f279ca728cfd6c
Taeyun Jeong <jty7231@naver.com>
2025-09-01 01:29:46 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int cnt = 0;
 
-void under_bar(int x)
+void underbar(int x)
 {
     for (int i = 0; i < x; i++)
         cout << "____";
@@ -11,29 +11,29 @@ void under_bar(int x)
 
 void what_is_recursive(int N)
 {
-    under_bar(cnt);
+    underbar(cnt);
     cout << "\"재귀함수가 뭔가요?\"" << endl;
 
     if (N == 0)
     {
-        under_bar(cnt);
+        underbar(cnt);
         cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << endl;
-        under_bar(cnt);
+        underbar(cnt);
         cout << "라고 답변하였지." << endl;
         return;
     }
 
-    under_bar(cnt);
+    underbar(cnt);
     cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << endl;
-    under_bar(cnt);
+    underbar(cnt);
     cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << endl;
-    under_bar(cnt);
+    underbar(cnt);
     cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << endl;
 
     cnt++;
     what_is_recursive(N - 1);
     cnt--;
-    under_bar(cnt);
+    underbar(cnt);
     cout << "라고 답변하였지." << endl;
 }
 
